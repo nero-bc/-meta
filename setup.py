8af8f8db502ea05c92066e3610cef730b8ddc4ec
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-17 15:04:34 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y python3-pip
+sudo pip3 install -U pip
+sudo apt-get install -y --no-install-recommends mediainfo ffmpeg
+sudo apt-get install libsox-fmt-mp3
+sudo apt-get install sox 
